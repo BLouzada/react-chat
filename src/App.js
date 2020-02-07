8e69fc2791773d0d40b3f04c246390e38110acf2
Bruno Louzada <bruno@ubots.com.br>
2020-02-07 13:25:23 -0300

    adicionando console de enviar mensagem

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
 import io from "socket.io-client";
-import Button from '@material-ui/core/Button';
 
-
-
+import MessageConsole from './MessageConsole'
 const socket = io('http://localhost:4200')
 socket.on('connect', () => console.log('[IO] Connect => A new connection has been established'))
 
 function App() {
-  const [color, setColor] = useState("white");
-  var send = (color) => {
-    console.log('send')
-    socket.emit('change color', color)
-  }
   return (
     <div style={{ textAlign: "center" }}>
-        <button onClick={send}>Change Color</button>
-        <Button variant="contained" color="primary">
-          Olá Mundo
-        </Button>
-
-        <button id="blue" onClick={() => send('blue')}>Blue</button>
-        <button id="red" onClick={() => send('red')}>Red</button>
-
+        <MessageConsole socket={socket}></MessageConsole>
       </div>
   );
 }
---
+++ src/MessageConsole.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: 200,
+      },
+    },
+  }));
+
+function MessageConsole(props) {
+    function send (event) {
+        event.preventDefault()
+        console.log('submeteu')
+        props.socket.emit('change color', 'color')
+      }
+    const classes = useStyles();
+    return (
+        <form className={classes.root} noValidate autoComplete="off" onSubmit={send}>
+          <TextField id="standard-basic" label="Standard" />
+          <Button type="submit" variant="contained" color="primary">
+            Enviar
+        </Button>
+        </form>
+      );
+}
+
+export default MessageConsole;
